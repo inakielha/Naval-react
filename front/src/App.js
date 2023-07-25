0e0f6b9600bbad319d6951983acef1343528f54d
inaki elhaiek <inakielhaiek@gmail.com>
2023-07-25 16:57:18 -0300

    test

--- front/src/App.js
+++ front/src/App.js
@@ -30,8 +30,8 @@ function App() {
    <Routes>
     {/* <Route path='/' element={<Navbar/>}/> */}
     {/* <Route path='/' element={<>{isMobile ? <Navbar/> : <NavbarWeb/>} <Mercury isMobile={isMobile} /> <Footer/></>}/> */}
-    {/* <Route path='/' element={<>{isMobile ? <NavMobileEcooter isMobile={isMobile}/> : <NavbarEcooter isMobile={isMobile}/>} <Ecooter isMobile={isMobile} /> <Footer color={"#6EC1E4"}/></>}/>
-    <Route path='/test' element={<>{isMobile ? <NavMobileEcooter/> : <NavbarEcooter/>} <EcooterE3 isMobile={isMobile} /> <Footer color={"#6EC1E4"}/></>}/> */}
+    <Route path='/' element={<>{isMobile ? <NavMobileEcooter isMobile={isMobile}/> : <NavbarEcooter isMobile={isMobile}/>} <Ecooter isMobile={isMobile} /> <Footer color={"#6EC1E4"}/></>}/>
+    <Route path='/test' element={<>{isMobile ? <NavMobileEcooter/> : <NavbarEcooter/>} <EcooterE3 isMobile={isMobile} /> <Footer color={"#6EC1E4"}/></>}/>
    </Routes>
    </>
   );
